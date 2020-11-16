7494e17ef8172ac1c1b3a1ec682fa1a940a6b35f
Shivakumar-hm <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2020-11-16 21:36:38 +0530

    new project

---
+++ HelloShivakumar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloShivakumar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
