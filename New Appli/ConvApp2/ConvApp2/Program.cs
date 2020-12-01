ca0cab1c7d949f9f09f82531b79a3a6010f4b844
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2020-12-01 16:13:22 +0530

    new Conv

--- New Appli/ConvApp2/ConvApp2/Program.cs
+++ New Appli/ConvApp2/ConvApp2/Program.cs
@@ -4,9 +4,14 @@ namespace ConvApp2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+       static void Main(string[] args) {
+         double d = 5673.74; 
+         int i;
+         
+         // cast double to int.
+         i = (int)d;
+         Console.WriteLine(i);
+         Console.ReadKey();
+      }
     }
 }
