cae3548c03cd531e563a66dc5a6ce747c0eb7b91
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2020-12-02 00:09:24 +0530

    new comment

--- New Appli/DataTypeApp/DataTypeApp/Program.cs
+++ New Appli/DataTypeApp/DataTypeApp/Program.cs
@@ -8,6 +8,9 @@ namespace DataTypeApp
         {
             //can find size of any datatype 
             Console.WriteLine("Size of int:{0}", sizeof(int));
+            Console.WriteLine("Size of char:{0}", sizeof(char));
+            Console.WriteLine("Size of double:{0}", sizeof(double));
+            Console.WriteLine("Size of float:{0}", sizeof(float));
             Console.ReadLine();
         }
     }
