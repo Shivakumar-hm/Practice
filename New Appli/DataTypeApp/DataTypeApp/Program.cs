4470af02b8d5e1764b012676a3ebd6c5649d0343
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2020-12-02 00:39:23 +0530

    new comment

--- New Appli/DataTypeApp/DataTypeApp/Program.cs
+++ New Appli/DataTypeApp/DataTypeApp/Program.cs
@@ -6,11 +6,15 @@ namespace DataTypeApp
     {
         static void Main(string[] args)
         {
+
+            object obj;
+            obj = 100;
             //can find size of any datatype 
             Console.WriteLine("Size of int:{0}", sizeof(int));
             Console.WriteLine("Size of char:{0}", sizeof(char));
             Console.WriteLine("Size of double:{0}", sizeof(double));
             Console.WriteLine("Size of float:{0}", sizeof(float));
+            Console.WriteLine("Value of obj:{0}", obj);
             Console.ReadLine();
         }
     }
